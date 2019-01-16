cd20d5aa14b329e3c8ee5bc9919b0a5d3c38aa63
Kaustubh <kkaustubh@us.ibm.com>
2019-01-16 14:57:34 -0800

    Added shell UI style and components on page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
+import { Search20, ShoppingCart20 } from '@carbon/icons-react';
+import {
+    Content,
+    Header,
+    HeaderMenuButton,
+    HeaderName,
+    HeaderNavigation,
+    HeaderMenuItem,
+    HeaderGlobalBar,
+    HeaderGlobalAction,
+    SkipToContent,
+} from 'carbon-components-react/lib/components/UIShell';
 import { Button } from 'carbon-components-react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <div>
-              <Button>React Carbon Component Button</Button>
-          </div>
-      </div>
+        <div>
+            <Header aria-label="Austin Tacos">
+                <SkipToContent />
+                <HeaderMenuButton aria-label="Open Menu" />
+                <HeaderName href="#" prefix="Austin">
+                    Tacos
+                </HeaderName>
+                <HeaderNavigation aria-label="Austin Tacos">
+                    <HeaderMenuItem href="#">Locations</HeaderMenuItem>
+                    <HeaderMenuItem href="#">Menu</HeaderMenuItem>
+                    <HeaderMenuItem href="#">Shop</HeaderMenuItem>
+                    <HeaderMenuItem href="#">Our Story</HeaderMenuItem>
+                    <HeaderMenuItem href="#">News</HeaderMenuItem>
+                </HeaderNavigation>
+                <HeaderGlobalBar>
+                    <HeaderGlobalAction aria-label="Search">
+                        <Search20 />
+                    </HeaderGlobalAction>
+                    <HeaderGlobalAction aria-label="Cart">
+                        <ShoppingCart20 />
+                    </HeaderGlobalAction>
+                </HeaderGlobalBar>
+            </Header>
+            <Content id="main-content">
+                Cart goes here.
+                <br />
+                <Button>React Carbon Component Button</Button>
+            </Content>
+        </div>
     );
   }
 }
