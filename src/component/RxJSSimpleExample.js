866c77bc46731360a4e723c508aa6fd5a6a269b3
Kaustubh <kkaustubh@us.ibm.com>
2019-01-18 15:12:43 -0800

    Refactored and rearranged observable examples.

--- src/component/RxJSSimpleExample.js
+++ src/component/RxJSSimpleExample.js
@@ -7,17 +7,7 @@ export class RxJSSimpleExample extends React.Component {
   constructor(props) {
     super(props);
     this.store = this.props.store;
-  }
 
-  handleUpdateObservable1 = () => {
-    this.store.dispatch(updateObservable1());
-  };
-
-  handleUpdateObservable2 = () => {
-    this.store.dispatch(updateObservable2());
-  };
-
-  render() {
     // [START: Example 1] parent - child observable and subscription.
     const parentObservable$ = interval (1000);
     const parentSubscription = parentObservable$.subscribe(x => console.log('Parent: '+x));
@@ -29,23 +19,46 @@ export class RxJSSimpleExample extends React.Component {
     // [END: Example 1]
 
 
-    // [START: Example 2] Create Observer
-    const observable2$ = Observable.create((observer) => {
-      observer.next(100);
-      observer.next(200);
-      observer.complete();
-    });
-    observable2$.subscribe(
-      value => console.log('Simple observable and subscription: '+value),
-      err => {},
-      () => console.log('REACHED TO END...')
-    );
-    // [END: Example 2]
-
     setTimeout(() => {
       parentSubscription.unsubscribe();
     }, 5000);
 
+    // [START: Example 2] Create Observer
+    this.observable2$ = Observable.create((observer) => {
+      observer.next(this.store.getState().rxJSSimpleExampleData);
+      observer.next(this.store.getState().rxJSSimpleExampleData);
+      observer.next(this.store.getState().rxJSSimpleExampleData);
+      observer.complete();
+    });
+    // [END: Example 2]
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentDidUpdate() {
+    // [START: Example 2] Subscribe Observer
+    this.observable2$.subscribe(
+      (rxJSSimpleExampleData) => {
+        rxJSSimpleExampleData.observable2 = rxJSSimpleExampleData.observable2 + 100;
+        console.log('Simple observable and subscription: ' + rxJSSimpleExampleData.observable2);
+      },
+      err => {},
+      () => console.log('REACHED TO END...')
+    );
+    // [END: Example 2]
+  }
+
+  handleUpdateObservable1 = () => {
+    this.store.dispatch(updateObservable1());
+  };
+
+  handleUpdateObservable2 = () => {
+    this.store.dispatch(updateObservable2());
+  };
+
+  render() {
     return (
       <div className="bx--grid">
         <div className="bx--row">
